676a587ac8d4038c1e5bd82cccf0b114b6ac3a15
indonswe <afc2002afc@hotmail.com>
2021-01-18 11:59:15 +0100

    working on 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,9 +10,28 @@ import java.util.List;
  */
 public class App 
 {
+    static List<String> weekDays;
     public static void main( String[] args )
     {
-        List<String> weekDays = new ArrayList<>(Arrays.asList("MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"));
-        System.out.println(weekDays);
+        //1
+        createList(1);
+        System.out.println(App.weekDays);
+
+        //2
+        createList(2);
+        for(String wDay:weekDays) {
+            System.out.println(wDay);
+        }
+
+        //3
+
+    }
+
+    public static void createList(int number){
+        if (number == 1 || number == 2) {
+            weekDays = new ArrayList<>(Arrays.asList("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"));
+        }else if (number == 3){
+            List<String> weekDays = new ArrayList<>(Arrays.asList("MONDAY","TUESDAY","WEDNESDAY","FRIDAY","SATURDAY","SUNDAY"));
+        }
     }
 }
