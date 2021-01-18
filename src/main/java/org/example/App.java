0b6ca5b03ab5f83d4fc524b631b6d25d3bb5cb1b
indonswe <afc2002afc@hotmail.com>
2021-01-18 11:31:27 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List<String> weekDays = new ArrayList<>(Arrays.asList("MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"));
+        System.out.println(weekDays);
+    }
+}
